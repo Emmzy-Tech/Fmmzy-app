b5941c27e6bc4413bf810961ec46bd6042959786
EmmzyTech <emmzytech@gmail.com>
2023-01-14 10:38:01 -0500

    API fetch using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
-function App() {
+//535f177d
+const API_URL = 'https://www.omdbapi.com?apikey=535f177d';
+
+
+const App = () => {
+  const fetchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+}
+useEffect(() => {
+  fetchMovies("Spiderman") 
+  }, [])
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>App</h1>
+);
 }
 
 export default App;
